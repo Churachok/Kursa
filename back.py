cee65002da4c222964739d95f1ba01545f85d15e
Churachok <kirillkonoplev510@gmail.com>
2026-04-24 13:54:54 +0700

    Pre-finished backend file
    Предварительно, бэк часть с логикой игры дописана. Я поправил некоторые элементы, например: разобрался в аргументами в функциях и методах, добавил пустую клетку. Закончил писать логику: добавил метод, проверки возможности передвижения фишки и проверку на победу

--- back.py
+++ back.py
@@ -1,13 +1,13 @@
 from random import randint
 from random import choice
 
-class GameLogic():
-    def rules():
-        self.is_win=[1,2,3,4,5,6,7,8]
+class GameLogic:
+    def rules(self):
+        self.is_win=[1,2,3,4,5,6,7,8,0]
         self.board = self.is_win_cp()
         self.empty_index = 8
 
-    def possible_moves():
+    def possible_moves(self):
         move=[]
         row,col = divmod(self.empty_index,3)
         if row>0:
@@ -20,16 +20,28 @@ class GameLogic():
             move.append(self.empty_index +1)
         return move
 
-    def swap(index):
+    def swap(self, index):
         self.board[self.empty_index], self.board = self.board[index], self.board[self.empty_index]
         self.empty_index = index
 
-    def start_game():
+    def start_game(self):
         self.board = self.is_win_cp()
-        self.wmpty_index = 8
+        self.empty_index = 8
         for i in range(100):
             move = self.possible_moves()
             if move:
                 random_move = choice(move)
                 self.swap(random_move)
+    
+    def is_moving(self, value):
+        if value not in range(1,9):
+            return False
+        index = self.board.index(value)
+        if index in self.possible_moves():
+            self.swap(index)
+            return True
+        return False
+
+    def win(self):
+        return self.board == self.is_win
         
