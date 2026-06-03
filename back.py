7df80a375febfe8c2aa3c68be1390052c02074e2
Churachok <kirillkonoplev510@gmail.com>
2026-06-03 23:39:19 +0700

    Update back.py

--- back.py
+++ back.py
@@ -2,28 +2,32 @@ from random import choice
 
 
 class GameLogic:
-    def __init__(self):
-        self.is_win = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    def __init__(self, size=3):
+        if size not in [3, 4, 5]:
+            raise ValueError("Размер должен быть 3, 4 или 5")
+        self.size = size
+        self.total_cells = size * size
+        self.is_win = list(range(1, self.total_cells)) + [0] 
         self.board = []
-        self.empty_index = 8
+        self.empty_index = self.total_cells - 1
 
     def is_win_cp(self):
         return self.is_win.copy()
 
     def rules(self):
         self.board = self.is_win_cp()
-        self.empty_index = 8
+        self.empty_index = self.total_cells - 1
 
     def possible_moves(self):
         moves = []
-        row, col = divmod(self.empty_index, 3)
+        row, col = divmod(self.empty_index, self.size)
         if row > 0:
-            moves.append(self.empty_index - 3)
-        if row < 2:
-            moves.append(self.empty_index + 3)
+            moves.append(self.empty_index - self.size)
+        if row < self.size - 1:
+            moves.append(self.empty_index + self.size)
         if col > 0:
             moves.append(self.empty_index - 1)
-        if col < 2:
+        if col < self.size - 1:
             moves.append(self.empty_index + 1)
         return moves
 
@@ -33,17 +37,28 @@ class GameLogic:
 
     def start_game(self):
         self.board = self.is_win_cp()
-        self.empty_index = 8
-        for i in range(100):
+        self.empty_index = self.total_cells - 1
+        shuffle_count = {
+            3: 100,
+            4: 200,
+            5: 300
+        }
+        
+        for i in range(shuffle_count.get(self.size, 100)):
             possible = self.possible_moves()
             if possible:
                 random_move = choice(possible)
                 self.swap(random_move)
 
     def is_moving(self, value):
-        if value not in range(1, 9):
+        if value not in range(1, self.total_cells):
             return False
-        index = self.board.index(value)
+        
+        try:
+            index = self.board.index(value)
+        except ValueError:
+            return False
+            
         if index in self.possible_moves():
             self.swap(index)
             return True
@@ -51,3 +66,9 @@ class GameLogic:
 
     def win(self):
         return self.board == self.is_win
+    
+    def get_size(self):
+        return self.size
+    
+    def get_board_2d(self):
+        return [self.board[i:i + self.size] for i in range(0, self.total_cells, self.size)]
